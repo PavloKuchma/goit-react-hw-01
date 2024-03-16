c35773241f45009c80530e54734d154a95ebef8f
PavloKuchma <111251902+PavloKuchma@users.noreply.github.com>
2024-03-17 00:43:10 +0200

    friendlist

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const FriendListItem = (avatar, name, isOnline, id) => {
-  return <div>FriendListItem</div>;
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img src={avatar} alt={name} width="48" />
+      <p>{name}</p>
+      <p>
+        Friend status: {isOnline ? <span>Online</span> : <span>Offline</span>}
+      </p>
+    </div>
+  );
 };
 
 export default FriendListItem;
